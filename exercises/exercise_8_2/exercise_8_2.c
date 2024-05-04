c75a08d3220a569b4a2f07da510e7ebea45399b1
ibtihal79 <168821667+ibtihal79@users.noreply.github.com>
2024-05-04 02:18:17 +0200

    Update exercise_8_2.c

--- exercises/exercise_8_2/exercise_8_2.c
+++ exercises/exercise_8_2/exercise_8_2.c
@@ -1,32 +1,27 @@
-#include <stdio.h>
 
-int main(void) {
+
+#include <stdio.h>
+void print(float[], int);
+
+int main() {
 
    int sum = 0;
-   int matrix[5][5] = {
-      4, 6, 25, 88, 5,
-      34, 5, 300, 4, 65,
-      78, 43, 11, 90, 125,
-      98, 585, 12, 63, 21,
-      45, 35, 9, 5, 1
+   int m[5][5] = {
+     { 4, 6, 25, 88, 5},
+      {34, 5, 300, 4, 65},
+     { 78, 43, 11, 90, 125},
+     { 98, 585, 12, 63, 21},
+      {45, 35, 9, 5, 1}
    };
 
-   printf("In the array:\n");
-
-   int i, j;
-   for (i = 0; i < 5; i++) {
-      for (j = 0; j < 5; j++) {
-         sum += matrix[i][j];
-         if (j == 4) {
-            printf("%d\n", matrix[i][j]);
-         } else {
-            printf("%d\t", matrix[i][j]);
-         }
-      }
-   }
-
-   printf("\n\nthe sum of the elements is %d", sum);
-
-   return 0;
-
+for(int k=0; k<5; k++){
+    for(int j=0; j<5; j++){
+        printf("%d\t", m[k][j]);
+        
+        sum+= m[k][j];
+    }
+    printf("\n");
+}
+printf("the sum of the elements is %d", sum);
+return 0;
 }
